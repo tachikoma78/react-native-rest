22c6e74e6a61f45dcde663eba2522e1f7a30e7c6
Pierre <pierrevanderpol@gmail.com>
2017-11-01 16:26:04 +0100

    added rest service and design for card

---
+++ albums/index.js
@@ -0,0 +1,17 @@
+//place code here fore iOS
+//import library to helpp craete compoennet
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// create component be careful it can only have ONE element
+const App = () => (
+    <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
+);
+
+//render to device
+AppRegistry.registerComponent('albums', () => App);
---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+    state = { albums: [] };
+
+    componentWillMount() {
+       // console.log('compoememt will mount in AlbulList');
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => this.setState({ albums: response.data }));
+          //  response => console.log(response)
+    }
+
+    renderAlbums() {
+       return this.state.albums.map(album => 
+       <AlbumDetail key={album.title} album={album} /> 
+       // I can call the prop as I want , like record insead of album
+        );
+    }
+    
+    render() {
+        console.log(this.state);
+
+       return (
+       <View> 
+             <View> 
+             {this.renderAlbums()} 
+             </View>
+       </View>
+       ); 
+    }
+}
+
+export default AlbumList;
